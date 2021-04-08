b74178efcefd6d95e489fc3fe9d00fbc469ef7fc
Thomas Syczyk <tomsycz@gmail.com>
2021-04-08 13:54:03 +0100

    navi update

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBars = document.getElementById("menu-bars");
+const ovarlay = document.getElementById("ovarlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+
+
+// Event listeners 
+
